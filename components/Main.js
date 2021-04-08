2d00897d11f3a8bc60ccf5791b0fccb0b0aecc5d
Gibbonus <vovjopan@gmail.com>
2021-04-08 16:59:06 +1000

    added camera and image picker

--- components/Main.js
+++ components/Main.js
@@ -3,13 +3,13 @@ import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser } from '../redux/actions/index';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import FeedScreen from './main/Feed';
 import MarerialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import ProfileScreen from './main/Profile';
 import AddScreen from './main/Add';
 
-const Tab = createBottomTabNavigator()
+const Tab = createMaterialBottomTabNavigator()
 const EmptyScreen = () => {
     return(null)
 }
@@ -20,7 +20,7 @@ export class MainScreen extends Component{
 
     render(){
         return(
-            <Tab.Navigator initialRouteName="Feed">
+            <Tab.Navigator initialRouteName="Feed" labeled={false}>
                 <Tab.Screen name="Feed" component={FeedScreen}
                 options={{
                     tabBarIcon: ({ size, color }) => (

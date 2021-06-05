a5a8a797a8270245891c8da62c5e762ef1acb06d
Gibbonus <vovjopan@gmail.com>
2021-06-05 19:43:58 +1000

    Changed message model so message deletes for both users

--- backend/inst_back/chat/models.py
+++ backend/inst_back/chat/models.py
@@ -2,10 +2,35 @@ from django.db import models
 from api.models import Profile
 
 class Chat(models.Model):
-    name = models.CharField(max_length = 50)
-    started = models.DateTimeField(auto_now_add = True)
+    started = models.DateTimeField(
+        auto_now_add = True)
+        
+    users = models.ManyToManyField(
+        Profile, 
+        related_name = 'chats')
 
 class Message(models.Model):
-    text = models.CharField(max_length = 250)
-    chat = models.ForeignKey(Chat, related_name = 'messages', on_delete=models.CASCADE)
-    users = models.ManyToManyField(Profile, related_name = 'messages')
+    text = models.CharField(
+        max_length = 250)
+
+    chat = models.ForeignKey(
+        Chat, 
+        related_name = 'messages', 
+        on_delete = models.CASCADE)
+        
+    sender = models.ForeignKey(
+        Profile, 
+        related_name = 'sended_messages', 
+        on_delete = models.CASCADE,
+        blank = True)
+
+    receiver = models.ForeignKey(
+        Profile, 
+        related_name = 'received_messages', 
+        on_delete = models.CASCADE,
+        blank = True)
+    
+    created = models.DateTimeField(editable = False, null = True)
+
+    class Meta:
+        ordering = ['created']

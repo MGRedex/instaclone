c028ccff9b0b8b13dd72132e6b2ab919e2b650e4
Gibbonus <vovjopan@gmail.com>
2021-06-09 15:05:39 +1000

    Added following system tests

--- backend/inst_back/api/tests.py
+++ backend/inst_back/api/tests.py
@@ -70,6 +70,48 @@ class AuthenticationTestCase(APITestCase):
         blacklisted_token = BlacklistedToken.objects.get()
         self.assertEqual(str(blacklisted_token.token.token), str(token))
 
+class FollowTestCase(APITestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        followee_user = User.objects.create_user(username = 'testuser1', password = '12345')
+        followee_profile = Profile.objects.create(user = followee_user, id = 1)
+
+        follower_user = User.objects.create_user(username = 'testuser2', password = '12345')
+        follower_profile = Profile.objects.create(user = follower_user, id = 2)
+
+        cls.authentication = JWTAuthentication()
+        cls.token = RefreshToken.for_user(follower_user)
+
+    def test_follow(self):
+        url = reverse('follow_unfollow', kwargs = {'pk': 1, 'action': 'follow'})
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {self.token.access_token}')
+        response = self.client.put(url)
+
+        self.assertEqual(response.status_code, 204)
+
+        followee_user = Profile.objects.get(id = 1)
+        follower_profile = Profile.objects.get(id = 2)
+
+        self.assertEqual(follower_profile.following.get(id = 1), followee_user)
+        self.assertEqual(followee_user.followers.get(id = 2), follower_profile)
+
+    def test_unfollow(self):
+        followee_user = Profile.objects.get(id = 1)
+        follower_profile = Profile.objects.get(id = 2)
+        follower_profile.following.add(followee_user)
+
+        url = reverse('follow_unfollow', kwargs = {'pk': 1, 'action': 'unfollow'})
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {self.token.access_token}')
+        response = self.client.put(url)
+
+        self.assertEqual(response.status_code, 204)
+
+        with self.assertRaisesMessage(Profile.DoesNotExist, 'Profile matching query does not exist'):
+            follower_profile.following.get(id = 1)
+
+        with self.assertRaisesMessage(Profile.DoesNotExist, 'Profile matching query does not exist'):
+            followee_user.followers.get(id = 2)
 class FeedTestCase(APITestCase):
     
 
